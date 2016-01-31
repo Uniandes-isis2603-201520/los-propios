e748b99c5f50a1ac20aeb1e609ed96b9ce98bfec
hj.calderon10 <hj.calderon10@172.24.99.234>
2016-01-31 15:36:30 -0500

    Modificado index, carpeta resources y creado el principal de forma basic.

--- mis-vacaciones/src/main/webapp/src/app.js
+++ mis-vacaciones/src/main/webapp/src/app.js
@@ -7,8 +7,12 @@
         }]);
 
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/book");
+            $urlRouterProvider.otherwise("/principal");
             $stateProvider
+                    .state('principal', {
+                        url: '/principal',
+                        templateUrl: "src/modules/principal/principal.tpl.html"
+                    })
                     .state('evento', {
                         url: '/evento',
                         templateUrl: "src/modules/evento/evento.tpl.html"
