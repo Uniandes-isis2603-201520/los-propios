6a770c293c6cac6911fe25466532fa9716192174
s.trujillo10 <s.trujillo10@uniandes.edu.co>
2016-02-11 04:12:15 -0500

    Ciudades

--- mis-vacaciones/src/main/webapp/resources/js/infoCiudad.js
+++ mis-vacaciones/src/main/webapp/resources/js/infoCiudad.js
@@ -9,12 +9,12 @@
 
 <!-- Acción sobre el botón con id=boton y actualizamos el div con id=itinerario -->
         function initialize() {
-        var mapProp = {
-        center:new google.maps.LatLng(48.8566667, 2.3509871),
-        zoom:5,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            var mapProp = {
+                center:new google.maps.LatLng(48.8566667, 2.3509871),
+                zoom:5,
+                mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
         }
         google.maps.event.addDomListener(window, 'load', initialize);
 
