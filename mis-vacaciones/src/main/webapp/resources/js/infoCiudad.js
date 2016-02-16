7262607fdd8aaf3e6f70a35a993fd875fbef1ed9
mc.hernandez1 <mc.hernandez1@uniandes.edu.co>
2016-02-16 04:33:24 -0500

    Cambios para el ui-router

--- mis-vacaciones/src/main/webapp/resources/js/infoCiudad.js
+++ mis-vacaciones/src/main/webapp/resources/js/infoCiudad.js
@@ -16,7 +16,6 @@
   };
   var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
--- mis-vacaciones/src/main/webapp/src/app.js
+++ mis-vacaciones/src/main/webapp/src/app.js
@@ -1,56 +1,82 @@
 (function (ng) {
 
-    var mod = ng.module("mainApp", ["ui.router"]);
-
-    mod.config(['$logProvider', function ($logProvider) {
-            $logProvider.debugEnabled(true);
+var mod = ng.module("mainApp", ["ui.router"]);
+        mod.config(['$logProvider', function ($logProvider) {
+        $logProvider.debugEnabled(true);
         }]);
-
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/principal");
-            $stateProvider
-                    .state('principal', {
-                        url: '/principal',
+        mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/principal");
+                $stateProvider
+                .state('principal', {
+                url: '/principal',
                         templateUrl: "src/modules/principal/principal.tpl.html"
-                    })
-                    .state('evento', {
-                        url: '/evento',
+                })
+                .state('evento', {
+                url: '/evento',
                         templateUrl: "src/modules/evento/evento.tpl.html"
-                    })
-                    .state('perfilUsuario', {
-                        url: '/perfil',
-                        templateUrl: "src/modules/perfilUsuario/perfilUsuario.html"
-                    })
-                    .state('ciudad', {
-                        url: '/ciudad',
+                })
+                .state('perfilUsuario', {
+                url: '/perfil',
+                        templateUrl: "src/modules/perfilUsuario/perfilUsuario.html",
+                        views: {
+                        '': {templateUrl: 'foro.tpl.html'},
+                        // the child views will be defined here (absolutely named)
+                        'filaUno@perfilUsuario': {
+                        template: 'hi',
+                        }
+
+                                }
+                })
+
+                .state('perfilUsuario.list', {
+                url: '/list',
+                templateUrl: 'foro.tpl.html',
+                template: 'hi',
+
+               })
+                .state('ciudad', {
+                url: '/ciudad',
                         templateUrl: "src/modules/ciudad/ciudad.tpl.html"
-                    })
-                    .state('itinerario', {
-                        url: '/itinerario',
+                })
+                .state('itinerario', {
+                url: '/itinerario',
                         templateUrl: "src/modules/itinerario/itinerario.html"
-                    })
-                    .state('foro', {
-                        url: '/foro',
+                })
+                .state('foro', {
+                url: '/foro',
                         templateUrl: "src/modules/foro/foro.tpl.html"
-                    })
-                    .state('contacto', {
-                        url: '/contacto',
+                })
+                .state('contacto', {
+                url: '/contacto',
                         templateUrl: "src/modules/contactenos/contactenos.tpl.html"
-                    })
+                })
 
-                    .state('infoCiudad', {
-                        url: '/infoCiudad',
+                .state('infoCiudad', {
+                url: '/infoCiudad',
                         templateUrl: "src/modules/ciudad/infoCiudad.tpl.html"
-                    })
-                    .state('servicios', {
-                        url: '/servicios',
+                })
+                .state('servicios', {
+                url: '/servicios',
                         templateUrl: "src/modules/servicios/servicios.tpl.html"
-                    })
-                    .state('about', {
-                        url: '/about',
+                })
+                .state('about', {
+                url: '/about',
                         templateUrl: "src/modules/about/about.tpl.html"
-                    })
-                    ;
+                })
 
-        }]);
-})(window.angular);
+//                .state('forito', {
+//                 templateUrl: 'for.tpl..html',
+//                 controller: function($scope){
+//                 $scope.myModal = [{ name: 'Alice' }, { name: 'Bob' }];
+//                  }
+//                  })
+//                .state('perfil', {
+//                templateUrl: 'perfilUsuario.html'
+//                });
+//
+//                  function MainCtrl($state){
+//                  $state.transitionTo('perfil');
+//                   }
+
+            }]);
+        })(window.angular);
