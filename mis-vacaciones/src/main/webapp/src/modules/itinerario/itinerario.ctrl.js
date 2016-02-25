7d766f9b6cd14c8eb4e9b515d79244ddc0002faa
jgmurillo10 <jg.murillo10@uniandes.edu.co>
2016-02-25 01:59:30 -0500

    srv y ctrl de itinerario agregado

--- mis-vacaciones/src/main/webapp/src/modules/itinerario/itinerario.ctrl.js
+++ mis-vacaciones/src/main/webapp/src/modules/itinerario/itinerario.ctrl.js
@@ -1,7 +1,39 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 
+
+(function (ng) {
+
+    // es parte del módulo "personModule"
+    var mod = ng.module("itinerarioModule");
+
+    // crea el controlador con dependencias a $scope y a personService
+    mod.controller("itinerarioCtrl", ["$scope", "itinerarioService", function ($scope, svc) {
+
+            // TODO: define los atributos en el scope
+            $scope.itinerarios = [];
+            $scope.idItinerario = "";
+            $scope.ciudad = "";
+            $scope.descripcion = "";
+
+            // TODO: define funciones que son invocadas desde la pantalla
+            // y que usan funciones definidas en el servicio
+            $scope.agregarItinerario = function () {
+                var itinerario = [$scope.idItinerario, $scope.ciudad, $scope.descripcion];
+                svc.saveRecord(itinerario);
+            };
+
+            $scope.listarItinerarios = function () {
+                return svc.fetchRecords().then(function (response)
+                {
+                    $scope.itinerarios = response.data;
+                })
+            };
+
+        }]);
+
+})(window.angular);
--- mis-vacaciones/src/main/webapp/src/modules/itinerario/itinerario.svc.js
+++ mis-vacaciones/src/main/webapp/src/modules/itinerario/itinerario.svc.js
@@ -1,7 +1,63 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+(function (ng) {
+
+    var mod = ng.module("perfilUsuarioModule");
+
+    mod.service("perfilUsuarioService", ["$http", "personContext", function ($http, context) {
+            /**
+             * Obtener la lista de persons.
+             * Hace una petición GET con $http a /persons para obtener la lista
+             * @returns {promise} promise para leer la respuesta del servidor}
+             * Devuelve una lista de objetos de persons con sus atributos
+             */
+            this.fetchRecords = function () {
+                return $http.get(context);
+            };
+
+            /**
+             * Obtener un registro de persons.
+             * Hace una petición GET a /persons/:id para obtener
+             * los datos de un registro específico de persons
+             * @param {number} id del registro a obtener
+             * @returns {promise} promise para leer la respuesta del servidor
+             * Devuelve un objeto de persons con sus atributos
+             */
+            this.fetchRecord = function (id) {
+                return $http.get(context + "/" + id);
+            };
+
+            /**
+             * Guardar un registro de un albumes.
+             * Si currentRecord tiene la propiedad id, hace un PUT a /perfilUsuario/:id con los
+             * nuevos datos de la instancia de albumes.
+             * Si currentRecord no tiene la propiedad id, se hace un POST a /albumes
+             * para crear el nuevo registro de albumes
+             * @param {object} currentRecord instancia de albumes a guardar/actualizar
+             * @returns {promise} promise para leer la respuesta del servidor
+             * Devuelve un objeto de albumes con sus datos incluyendo el id
+             */
+            this.saveRecord = function (currentRecord) {
+                if (currentRecord.id) {
+                    return $http.put(context + "/" + currentRecord.id, currentRecord);
+                } else {
+                    return $http.post(context, currentRecord);
+                }
+            };
+
+            /**
+             * Hace una petición DELETE a /persons/:id para eliminar una person
+             * @param {number} id identificador de la instancia de person a eliminar
+             * @returns {promise} promise para leer la respuesta del servidor
+             * No devuelve datos.
+             */
+            this.deleteRecord = function (id) {
+                return $http.delete(context + "/" + id);
+            };
+        }]);
+})(window.angular);
 
