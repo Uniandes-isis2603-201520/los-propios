ee82f2440cb98de844860b608d4b2523f00c6069
js.gomez14 <js.gomez14@uniandes.edu.co>
2016-03-02 11:15:51 -0500

    Evento Resource

---
+++ mis-vacaciones/src/main/java/edu/uniandes/lospropios/resources/EventResource.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniandes.lospropios.resources;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+/**
+ *
+ * @author js.gomez14
+ */
+@Path("eventos")
+@Produces("application/json")
+public class EventResource
+{
+    
+
+
+}
