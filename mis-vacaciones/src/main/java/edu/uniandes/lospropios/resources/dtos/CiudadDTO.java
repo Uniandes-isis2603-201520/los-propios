5518d71b73bddb518e8ae49a654d3fa2370a66b5
s.trujillo10 <s.trujillo10@uniandes.edu.co>
2016-03-03 03:42:38 -0500

    DTO y Mock

---
+++ mis-vacaciones/src/main/java/edu/uniandes/lospropios/resources/dtos/CiudadDTO.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniandes.lospropios.resources.dtos;
+
+/**
+ *
+ * @author s.trujillo10
+ */
+public class CiudadDTO {
+
+    private Long id;
+    private String name;
+
+    /**
+     * Constructor por defecto
+     */
+    public CiudadDTO() {
+	}
+
+    /**
+     * Constructor con parámetros.
+     * @param id identificador de la ciudad
+     * @param name nombre de la ciudad
+     */
+    public CiudadDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Convierte el objeto a una cadena
+     */
+    @Override
+    public String toString() {
+    	return "{ id : " + getId() + ", name : \"" + getName() + "\" }" ;
+    }
+}
