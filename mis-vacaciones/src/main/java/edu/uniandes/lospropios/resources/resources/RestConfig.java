5c46d5b0a745e670e7ae1235e0fa527442bab5e1
hj.calderon10 <hj.calderon10@uniandes.edu.co>
2016-03-03 10:28:39 -0500

    fix

---
+++ mis-vacaciones/src/main/java/edu/uniandes/lospropios/resources/resources/RestConfig.java
@@ -0,0 +1,14 @@
+/*
+ * RestConfig.java
+ * Configura el servicio JAX-RS.
+ */
+package edu.uniandes.lospropios.resources.resources;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+/**
+ *
+ * @author hj.calderon10
+ */
+@ApplicationPath("/api")
+public class RestConfig extends Application { }
--- mis-vacaciones/src/main/webapp/src/app.js
+++ mis-vacaciones/src/main/webapp/src/app.js
@@ -9,171 +9,110 @@
         "perfilUsuarioMock"
     ]);
 
-    var sesion = true;
     mod.config(['$logProvider', function ($logProvider) {
             $logProvider.debugEnabled(true);
         }]);
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.otherwise("/principal");
-            if (sesion)
-            {
-                $stateProvider
-                        .state('principal', {
-                            url: '/principal',
-                            views: {
-                                '': {templateUrl: "src/modules/principal/principal.tpl.html"},
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+            $stateProvider
+                    .state('principal', {
+                        url: '/principal',
+                        views: {
+                            '': {templateUrl: "src/modules/principal/principal.tpl.html"},
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
 
-                            }
-                        })
-                        .state('evento', {
-                            url: '/evento',
-                            views: {
-                                '': {
-                                    controller: "eventoCtrl",
-                                    controllerAs: "ctrl",
-                                    templateUrl: "src/modules/evento/evento.tpl.html"},
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
-                        .state('perfilUsuario', {
-                            url: '/perfil',
-                            views: {
-                                '': {
-                                    controller: "perfilUsuarioCtrl",
-                                    controllerAs: "ctrl",
-                                    templateUrl: "src/modules/perfilUsuario/perfilUsuario.html"},
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
+                        }
+                    })
+                    .state('evento', {
+                        url: '/evento',
+                        views: {
+                            '': {
+                                controller: "eventoCtrl",
+                                controllerAs: "ctrl",
+                                templateUrl: "src/modules/evento/evento.tpl.html"},
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
+                    .state('perfilUsuario', {
+                        url: '/perfil',
+                        views: {
+                            '': {
+                                controller: "perfilUsuarioCtrl",
+                                controllerAs: "ctrl",
+                                templateUrl: "src/modules/perfilUsuario/perfilUsuario.html"},
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
 
-                        .state('ciudad', {
-                            url: '/ciudad',
-                            views: {
-                                '': {
-                                    templateUrl: "src/modules/ciudad/ciudad.tpl.html"
-                                },
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
-                        .state('itinerario', {
-                            url: '/itinerario',
-                            views: {
-                                '': {
-                                    controller: "itinerarioCtrl",
-                                    controllerAs: "ctrl",
-                                    templateUrl: "src/modules/itinerario/itinerario.tpl.html"
-                                },
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
-                        .state('foro', {
-                            url: '/foro',
-                            views: {
-                                '': {
-                                    templateUrl: "src/modules/foro/foro.tpl.html"
-                                },
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
-                        .state('contacto', {
-                            url: '/contacto',
-                            views: {
-                                '': {
-                                    /* controller: "contactoCtrl",
-                                     controllerAs: "ctrl",*/
-                                    templateUrl: "src/modules/contactenos/contactenos.tpl.html"
-                                },
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
+                    .state('ciudad', {
+                        url: '/ciudad',
+                        views: {
+                            '': {
+                                templateUrl: "src/modules/ciudad/ciudad.tpl.html"
+                            },
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
+                    .state('itinerario', {
+                        url: '/itinerario',
+                        views: {
+                            '': {
+                                controller: "itinerarioCtrl",
+                                controllerAs: "ctrl",
+                                templateUrl: "src/modules/itinerario/itinerario.tpl.html"
+                            },
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
+                    .state('foro', {
+                        url: '/foro',
+                        views: {
+                            '': {
+                                templateUrl: "src/modules/foro/foro.tpl.html"
+                            },
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
+                    .state('contacto', {
+                        url: '/contacto',
+                        views: {
+                            '': {
+                                /* controller: "contactoCtrl",
+                                 controllerAs: "ctrl",*/
+                                templateUrl: "src/modules/contactenos/contactenos.tpl.html"
+                            },
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
 
-                        .state('infoCiudad', {
-                            url: '/infoCiudad',
-                            views: {
-                                '': {
-                                    templateUrl: "src/modules/ciudad/infoCiudad.tpl.html"},
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
-                        .state('servicios', {
-                            url: '/servicios',
-                            views: {
-                                '': {
-                                    templateUrl: "src/modules/servicios/servicios.tpl.html"
-                                },
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        })
-                        .state('about', {
-                            url: '/about',
-                            views: {
-                                '': {
-                                    /* controller: "aboutCtrl",
-                                     controllerAs: "ctrl",*/
-                                    templateUrl: "src/modules/about/about.tpl.html"
-                                },
-                                'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
-                            }
-                        });
-            } else
-            {
-                $stateProvider
-                        .state('principal', {
-                            url: '/principal',
-                            templateUrl: "src/modules/principal/principal.tpl.html"
-                        })
-                        .state('evento', {
-                            url: '/evento',
-                            templateUrl: "src/modules/evento/evento.tpl.html",
-                            controller: "eventoCtrl",
-                            controllerAs: "ctrl",
-                        })
-                        .state('perfilUsuario', {
-                            url: '/perfil',
-                            controller: "perfilUsuarioCtrl",
-                            controllerAs: "ctrl",
-                            templateUrl: "src/modules/perfilUsuario/perfilUsuario.html",
-                        })
-
-                        .state('ciudad', {
-                            url: '/ciudad',
-                            controller: "eventoCtrl",
-                            controllerAs: "ctrl",
-                            templateUrl: "src/modules/ciudad/ciudad.tpl.html"
-                        })
-                        .state('itinerario', {
-                            url: '/itinerario',
-                            controller: "itinerarioCtrl",
-                            controllerAs: "ctrl",
-                            templateUrl: "src/modules/itinerario/itinerario.tpl.html"
-                        })
-                        .state('foro', {
-                            url: '/foro',
-                            templateUrl: "src/modules/foro/foro.tpl.html"
-                        })
-                        .state('contacto', {
-                            url: '/contacto',
-                            /* controller: "contactoCtrl",
-                             controllerAs: "ctrl",*/
-                            templateUrl: "src/modules/contactenos/contactenos.tpl.html"
-                        })
-
-                        .state('infoCiudad', {
-                            url: '/infoCiudad',
-                            templateUrl: "src/modules/ciudad/infoCiudad.tpl.html"
-                        })
-                        .state('servicios', {
-                            url: '/servicios',
-                            templateUrl: "src/modules/servicios/servicios.tpl.html"
-                        })
-                        .state('about', {
-                            url: '/about',
-                            /* controller: "aboutCtrl",
-                             controllerAs: "ctrl",*/
-                            templateUrl: "src/modules/about/about.tpl.html"
-                        });
-            }
+                    .state('infoCiudad', {
+                        url: '/infoCiudad',
+                        views: {
+                            '': {
+                                templateUrl: "src/modules/ciudad/infoCiudad.tpl.html"},
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
+                    .state('servicios', {
+                        url: '/servicios',
+                        views: {
+                            '': {
+                                templateUrl: "src/modules/servicios/servicios.tpl.html"
+                            },
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    })
+                    .state('about', {
+                        url: '/about',
+                        views: {
+                            '': {
+                                /* controller: "aboutCtrl",
+                                 controllerAs: "ctrl",*/
+                                templateUrl: "src/modules/about/about.tpl.html"
+                            },
+                            'barraLateral': {templateUrl: "src/modules/principal/prueba.tpl.html"}
+                        }
+                    });
 
         }]);
 })(window.angular);
