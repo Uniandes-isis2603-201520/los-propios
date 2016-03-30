221abaf30a92118e1b0256b65de1e6fe2805e104
hj.calderon10 <hj.calderon10@uniandes.edu.co>
2016-03-30 10:52:56 -0500

    Tercer proyecto

---
+++ mis-vacaciones-logic/src/main/java/co/edu/uniandes/mis/vacaciones/logic/api/IinterfaceLogic.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.mis.vacaciones.logic.api;
+
+/**
+ *
+ * @author hj.calderon10
+ */
+public class IinterfaceLogic {
+
+}
