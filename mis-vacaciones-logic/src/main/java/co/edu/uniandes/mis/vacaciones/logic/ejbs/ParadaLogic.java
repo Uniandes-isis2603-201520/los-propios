d0834d2573db14e9c60f4a046fda2bc731ec3e6d
mc.hernandez1 <mc.hernandez1@uniandes.edu.coc>
2016-04-03 15:47:24 -0500

    Todo lo necesario para crear ParadaLogic

--- mis-vacaciones-logic/src/main/java/co/edu/uniandes/mis/vacaciones/logic/ejbs/ParadaLogic.java
+++ mis-vacaciones-logic/src/main/java/co/edu/uniandes/mis/vacaciones/logic/ejbs/ParadaLogic.java
@@ -7,13 +7,13 @@ package co.edu.uniandes.mis.vacaciones.logic.ejbs;
 
 import co.edu.uniandes.mis.vacaciones.api.IParadaLogic;
 
-import co.edu.uniandes.mis.vacaciones.entities.ItinerarioEntity;
+import co.edu.uniandes.mis.vacaciones.entities.VisitaEntity;
 
 import co.edu.uniandes.mis.vacaciones.entities.ParadaEntity;
 
 import co.edu.uniandes.mis.vacaciones.exceptions.BusinessLogicException;
 
-import co.edu.uniandes.mis.vacaciones.persistence.ItinerarioPersistence;
+import co.edu.uniandes.mis.vacaciones.persistence.VisitaPersistence;
 
 import co.edu.uniandes.mis.vacacaiones.persistence.ParadaPersistence;
 
@@ -33,81 +33,137 @@ import javax.inject.Inject;
  *
  * @author mc.hernandez1
  */
-
 @Stateless
 
-public class ParadaLogic implements IParadaLogic
+public class ParadaLogic implements IParadaLogic {
 
-{
+    private static final Logger logger = Logger.getLogger(ParadaLogic.class.getName());
 
-private static final Logger logger = Logger.getLogger (ParadaLogic.class.getName());
+    @Inject
+    private ParadaPersistence persistence;
 
-@Inject
-private ParadaPersistence persistence;
+    @Inject
+    private VisitaPersistence visitaPersistence;
 
-@Inject
-private ItinerarioPersistence itinerarioPersistence;
+    @Override
+    public List<ParadaEntity> getParadas() {
+        logger.info("Inicia el proceso de consultar todas las paradas");
+        List<ParadaEntity> paradas = persistence.findAll();
+        logger.info("Termina proceso de consultar todos los libros");
+        return paradas;
+    }
 
-@Override
-public List<ParadaEntity> getParadas()
-{
-    logger.info("Inicia el proceso de consultar todas las paradas");
-    List<ParadaEntity> paradas = persistence.findAll();
-    logger.info("Termina proceso de consultar todos los libros");
-    return paradas;
-}
+    @Override
+    public ParadaEntity getParada(Long id) throws BusinessLogicException {
+        logger.log(Level.INFO, "Inicia proceso de consultar parada con id = {0}", id);
+        ParadaEntity parada = persistence.find(id);
 
-@Override
-public ParadaEntity getParada(Long id) throws BusinessLogicException
-{
-    logger.log(Level.INFO, "Inicia proceso de consultar parada con id = {0}", id);
-    ParadaEntity parada = persistence.find(id);
+        if (parada == null) {
+            logger.log(Level.SEVERE, "La parada con el id {0} no existe", id);
+            throw new BusinessLogicException("La parada solicitada no existe");
 
-    if(parada == null)
-    {
-        logger.log(Level.SEVERE, "La parada con el id {0} no existe", id);
-        throw new BusinessLogicException ("La parada solicitada no existe");
+        }
+
+        logger.log(Level.INFO, "Termina proceso de consultar parada con id = {0}", id);
+        return parada;
+    }
+
+    @Override
+    public ParadaEntity createParada(ParadaEntity entity) throws BusinessLogicException {
+        logger.info("Inicia proceso de creación de parada");
+        if (!validateISBN(entity.getIsbn())) {
+            throw new BusinessLogicException("El ISBN es inválido");
+        }
+        persistence.create(entity);
+        logger.info("Termina proceso de creación de parada");
+        return entity;
+    }
+
+    @Override
+    public ParadaEntity updateParada(ParadaEntity entity) throws BusinessLogicException {
+        logger.log(Level.INFO, "Inicia proceso de actualizar parada con id = {0}", entity.getId());
+        if (!validateISBN(entity.getIsbn())) {
+            throw new BusinessLogicException("El ISBN es inválido");
+        }
+        ParadaEntity newEntity = persistence.update(entity);
+        logger.log(Level.INFO, "Termina proceso de actualizar parada con id={0}", entity.getId());
+        return newEntity;
+    }
+
+    @Override
+    public void deleteParada(Long id) {
+        logger.log(Level.INFO, "Inicia proceso de borrar parada con id = {0}", id);
+        persistence.delete(id);
+        logger.log(Level.INFO, "Temrmina proceso de borrar parada con id ={0}", id);
 
     }
 
-    logger.log(Level.INFO, "Termina proceso de consultar parada con id = {0}", id);
-    return parada;
-}
-
-@Override
-public ParadaEntity createParada(ParadaEntity entity) throws BusinessLogicException
-{
-    logger.info("Inicia proceso de creación de parada");
-    if(!validateISBN(entity.getIsbn()))
-    {
-        throw new BusinessLogicException("El ISBN es inválido");
+    @Override
+    public List<VisitaEntity> getVisitas(Long visitaId) {
+        return persistence.find(visitaId).getVisitas();
     }
-    persistence.create(entity);
-    logger.info("Termina proceso de creación de parada");
-    return entity;
-}
 
-@Override
-public ParadaEntity updateParada (ParadaEntity entity)throws BusinessLogicException
-{
-    logger.log(Level.INFO,"Inicia proceso de actualizar parada con id = {0}",entity.getId());
-    if(!validateISBN(entity.getIsbn()))
-    {
-        throw new BusinessLogicException("El ISBN es inválido");
+    @Override
+    public VisitaEntity getVisita(Long paradaId, Long visitaId) {
+        List<VisitaEntity> visitas = persistence.find(paradaId).getVisitas();
+        VisitaEntity visitaEntity = new VisitaEntity();
+        visitaEntity.setId(visitaId);
+        int index = visitas.indexOf(visitaEntity);
+        if (index >= 0) {
+            return visitas.get(index);
+        }
+        return null;
     }
-    ParadaEntity newEntity = persistence.update(entity);
-    logger.log(Level.INFO, "Termina proceso de actualizar parada con id={0}", entity.getId());
-    return newEntity;
-}
 
-@Override
-public void deleteParada(Long id)
-{
-    logger.log(Level.INFO,"Inicia proceso de borrar parada con id = {0}", id);
-    persistence.delete(id);
-    logger.log(Level.INFO,"Temrmina proceso de borrar parada con id ={0}", id);
+    @Override
+    public VisitaEntity addVisita(Long visitaId, Long paradaId) throws BusinessLogicException {
+        ParadaEntity paradaEntity = persistence.find(paradaId);
+        VisitaEntity visitaEntity = visitaPersistence.find(visitaId);
+        if (!bornBeforePublishDate(visitaEntity.getBirthdate(), paradaEntity.getPublishDate()));
+        {
+            throw new BusinessLogicException("La fecha de publicación no puede der anterior a la fecha en que se creo la parada");
+        }
+        paradaEntity.getVisitas().add(visitaEntity);
+        return visitaEntity;
+    }
 
-}
+    @Override
+    public void removeVisita(Long visitaId, Long paradaId) {
+        ParadaEntity paradaEntity = persistence.find(paradaId);
+        VisitaEntity visitaEntity = new VisitaEntity();
+        visitaEntity.setId(visitaId);
+        paradaEntity.getVisitas().remove(visitaEntity);
+    }
+
+    @Override
+    public List<VisitaEntity> replaceVisitas(List<VisitaEntity> visitas, Long paradaId) throws BusinessLogicException {
+        ParadaEntity paradaEntity = persistence.fing(paradaId);
+        paradaEntity.setVisitas(visitas);
+        for (VisitaEntity visita : visitas) {
+            if (!bornBeforePublishDate(visita.getBirthDate(), paradaEntity.getPublishDate())) {
+                throw new BusinessLogicException("La fecha de publicación no puede ser anterior a la fecha en que se creo la parada");
+            }
+        }
+    }
+
+    private boolean validateISBN(String isbn) {
+        if (isbn == null || isbn.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean bornBeforePublishDate(Date birthDate, Date publishDate)
+    {
+        if (publishDate != null && birthDate != null)
+        {
+            if (birthDate.before(publishDate))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
--- mis-vacaciones-logic/src/main/java/co/edu/uniandes/mis/vacaciones/logic/entities/ParadaEntity.java
+++ mis-vacaciones-logic/src/main/java/co/edu/uniandes/mis/vacaciones/logic/entities/ParadaEntity.java
@@ -6,6 +6,7 @@
 package co.edu.uniandes.mis.vacaciones.logic.entities;
 
 import co.edu.uniandes.csw.crud.spi.entity.BaseEntity;
+import
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,6 +18,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import co.edu.uniandes.mis.vacaciones.entities.VisitaEntity;
 /**
  *
  * @author mc.hernandez1
@@ -25,5 +27,87 @@ import javax.persistence.TemporalType;
 
 public class ParadaEntity extends BaseEntity implements Serializable
 {
-private String
+private String isbn;
+private String image;
+@Temporal(TermporalType.DATE)
+private Date publishdate;
+private String descripcion;
+
+@ManyToMany
+private List<VisitaEntity> visitas = new ArrayList();
+
+@ManyToMany
+private ItinerarioEntity itinerario;
+
+@OneToMany(mappedBy = "parada", cascade= CascadeType.ALL, orphanRemoval = true)
+private List<ReviewEntity> reviews = new ArrayList();
+
+/**
+ * @return the isbn
+ */
+public String getIsbn()
+{
+    return isbn;
 }
+
+/**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the publishDate
+     */
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    /**
+     * @param publishDate the publishDate to set
+     */
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<VisitaEntity> getVisitas()
+    {
+        return visitas;
+    }
+
+    public void setVisitas(List<VisitaEntity> visitas)
+    {
+        this.visitas = visitas;
+    }
+    
+}
+
