ad748ff0e9e3060f941425f4938b8109ec836099
mc.hernandez1 <mc.hernandez1@uniandes.edu.co>
2016-05-08 09:00:27 -0500

    Cambios en la creacion de parada .mod además de la inyección de este modulo

--- mis-vacaciones/src/main/webapp/src/modules/parada/parada.mod.js
+++ mis-vacaciones/src/main/webapp/src/modules/parada/parada.mod.js
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-(function (ng) {
 
-    var mod = ng.module("itinerarioModule", ["ui.bootstrap"]);
-    mod.constant("itinerarioContext", "api/itinerarios");
+(function (ng) {
+    var mod = ng.module("paradaModule", ["ui.bootstrap"]);
+    mod.constant("paradaContext", "api/paradas");
 
 
 })(window.angular);
